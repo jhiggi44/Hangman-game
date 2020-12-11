2b734d93b0d79a35dd2213b64e05121b2fc3d7f4
Jordan Higgins <jordanhiggins@Mavenlinks-MacBook-Pro.local>
2020-12-11 15:57:01 -0700

    Get beginGame in a test harness

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,23 @@
 import { Hint } from "./hint.js";
 
-// the list of spells for the user to guess
-let spellsArray = ["alohomora", 
-"avada kedavra", "confundo","expelliarmus",
-"levicorpus", "morsmordre", "obliviate", 
-"prior incantato", "riddikulus"];
-
-// spell variables
-let secretSpell;
-let lettersInSpell;
-let letterMask = [];
-
-let numLettersLeft;
-let lettersGuessed = [];
-
-// game variables
+let secretSpellDisplay;	
+let guessesLeftDisplay;
+let winsDisplay;
+let lettersGuessedDisplay;
+let gameVariables;
 let winCount = 0;
 
-function beginGame () {
-	lettersGuessed = [];
 
+function beginGame(words) {
+	console.log("begin game.");
 	// initialize spell/word variables
-	letterMask = [];
-	secretSpell = spellsArray[Math.floor(Math.random() * spellsArray.length)];
-	lettersInSpell = secretSpell.split("");
+	const spellsArray = words || ["alohomora", 
+	"avada kedavra", "confundo","expelliarmus",
+	"levicorpus", "morsmordre", "obliviate", 
+	"prior incantato", "riddikulus"];
+
+	const secretSpell = spellsArray[Math.floor(Math.random() * spellsArray.length)];
+	const lettersInSpell = secretSpell.split("");
 
 	// counts spaces - the number of spaces indicates the spell is more than one word long
 	let numSpaces = 0;
@@ -34,8 +28,9 @@ function beginGame () {
 	}
 
 	// the numLettersLeftToGuess equals the length lettersInSpell minus the spaces
-	numLettersLeft = lettersInSpell.length - numSpaces;
+	let numLettersLeft = lettersInSpell.length - numSpaces;
 
+	let letterMask = [];
 	// here we create the mask, if the letter is a space push a <br> tag into the array, 
 	// which will seperate words when using innerHTML
 	for (let i = 0; i < lettersInSpell.length; i++){
@@ -48,93 +43,102 @@ function beginGame () {
 
 	return {
 		guessesLeft: 11,
+		lettersGuessed: [],
+		secretSpell,
+		lettersInSpell,
+		letterMask,
+		numLettersLeft,
+	};
+}
+
+function updateLetterMask(letter) {
+	for (var i = 0; i < gameVariables.lettersInSpell.length; i++) {
+		if(gameVariables.secretSpell[i] == letter){
+			gameVariables.letterMask[i] = letter;
+		}
 	}
 }
 
+function updateNumLettersLeft(letter) {
+	for (var i = 0; i < gameVariables.lettersInSpell.length; i++) {
+		if(gameVariables.secretSpell[i] == letter){
+			gameVariables.numLettersLeft--;
+		}
+	}
+}
+
+function updateLettersGuessed(letter) {
+	gameVariables.lettersGuessed.push(letter);
+}
+
+function letterCheck (letter) {
+	let isLetterInWord = false;
+	for (let i = 0; i < gameVariables.letterMask.length; i++) {
+		if (gameVariables.secretSpell[i] == letter) {
+			isLetterInWord = true;
+		}
+	}
+
+	let hasLetterBeenGuessed = false;
+	for (let i = 0; i < gameVariables.lettersGuessed.length; i++) {
+		if (gameVariables.lettersGuessed[i] == letter) {
+			hasLetterBeenGuessed = true;
+		}
+	}
+
+	//Checks if letter is in spell, then sends it to corresponding blank
+	if(isLetterInWord && !hasLetterBeenGuessed) {
+		updateNumLettersLeft(letter);
+		updateLettersGuessed(letter);
+		updateLetterMask(letter);
+
+		console.log("letters left #: " + numLettersLeft);
+		} else if (!hasLetterBeenGuessed) { 
+			gameVariables.lettersGuessed.push(letter);
+			gameVariables.guessesLeft--;
+		}
+}
+
+function finishRound() {
+	// update HTML with current stats
+	console.log(gameVariables);
+	guessesLeftDisplay.innerHTML = gameVariables.guessesLeft;
+	secretSpellDisplay.innerHTML = gameVariables.letterMask.join(" ");
+	lettersGuessedDisplay.innerHTML = gameVariables.lettersGuessed.join(" ");
+
+	//Did user win?
+	if (gameVariables.numLettersLeft == 0) {
+		winCount++
+		alert(`You're a great wizard! The spell was ${gameVariables.secretSpell}`);
+		winsDisplay.innerHTML = winCount
+		gameVariables = beginGame();
+	} else if (gameVariables.guessesLeft == 0){
+		alert("Are you sure you're a wizard");
+		gameVariables = beginGame();
+	}
+}
+
+function pageInit(letterMask, guessesLeft) {
+	// update HTML page
+	secretSpellDisplay.innerHTML = letterMask.join(" ");
+	guessesLeftDisplay.innerHTML = guessesLeft;
+	winsDisplay.innerHTML = winCount;
+}	
+
+
 window.onload = function() {
+
+	console.log("beginning game...");
 	// DOM variables 
-	let secretSpellDisplay = document.getElementById("guessSpell");	
-	let guessesLeftDisplay = document.getElementById("guessRemain");
-	let winsDisplay = document.getElementById("numberWins");
-	let lettersGuessedDisplay = document.getElementById("lettersGuessed");
+	secretSpellDisplay = document.getElementById("guessSpell");	
+	guessesLeftDisplay = document.getElementById("guessRemain");
+	winsDisplay = document.getElementById("numberWins");
+	lettersGuessedDisplay = document.getElementById("lettersGuessed");
 
-	function pageInit(letterMask, guessesLeft, winCount) {
-		// update HTML page
-		secretSpellDisplay.innerHTML = letterMask.join(" ");
-		guessesLeftDisplay.innerHTML = guessesLeft;
-		winsDisplay.innerHTML = winCount;
-	}
-
-	let { guessesLeft } = beginGame();
-	pageInit(letterMask, guessesLeft, winCount);
-
-	function updateLetterMask(letter) {
-		for (var i = 0; i < lettersInSpell.length; i++) {
-			if(secretSpell[i] == letter){
-				letterMask[i] = letter;
-			}
-		}
-	}
-
-	function updateNumLettersLeft(letter) {
-		for (var i = 0; i < lettersInSpell.length; i++) {
-			if(secretSpell[i] == letter){
-				numLettersLeft--;
-			}
-		}
-	}
-
-	function updateLettersGuessed(letter) {
-		lettersGuessed.push(letter);
-	}
-
-	async function letterCheck (letter) {
-		
-		let isLetterInWord = false;
-		for (let i = 0; i < letterMask.length; i++) {
-			if (secretSpell[i] == letter) {
-				isLetterInWord = true;
-			}
-		}
-
-		let hasLetterBeenGuessed = false;
-		for (let i = 0; i < lettersGuessed.length; i++) {
-			if (lettersGuessed[i] == letter) {
-				hasLetterBeenGuessed = true;
-			}
-		}
-
-		//Checks if letter is in spell, then sends it to corresponding blank
-		if(isLetterInWord && !hasLetterBeenGuessed) {
-			updateNumLettersLeft(letter);
-			updateLettersGuessed(letter);
-			updateLetterMask(letter);
-
-			console.log("letters left #: " + numLettersLeft);
-			} else if (!hasLetterBeenGuessed) { 
-				lettersGuessed.push(letter);
-				guessesLeft--
-			}
-	}
-
-	function finishRound() {
-
-		// update HTML with current stats
-		guessesLeftDisplay.innerHTML = guessesLeft;
-		secretSpellDisplay.innerHTML = letterMask.join(" ");
-		lettersGuessedDisplay.innerHTML = lettersGuessed.join(" ");
-
-		//Did user win?
-		if (numLettersLeft == 0) {
-			winCount++
-			alert(`You're a great wizard! The spell was ${secretSpell}`);
-			winsDisplay.innerHTML = winCount
-			beginGame();
-		} else if (guessesLeft == 0){
-			alert("Are you sure you're a wizard");
-			beginGame();
-		}
-	}
+	console.log("beginning game...");
+	gameVariables = beginGame();
+	
+	pageInit(gameVariables.letterMask, gameVariables.guessesLeft);
 
 	document.addEventListener("keyup", () => {
 		let userGuess = String.fromCharCode(event.keyCode).toLowerCase();
@@ -148,7 +152,6 @@ window.onload = function() {
 		updateLetterMask(hint);
 		updateNumLettersLeft(hint);
 		updateLettersGuessed(hint);
-
 		finishRound();
 	});
 }
--- tests/game.test.js
+++ tests/game.test.js
@@ -2,12 +2,33 @@ import { beginGame } from "../assets/javascript/game";
 
 describe('beginning a game', () => {
     let gameVariables;
+    const words = ["foo"];
 
     beforeEach(() => {
-        gameVariables = beginGame();
+        gameVariables = beginGame(words);
     });
 
-    test('sets guessesLeft variable', () => {
+    test('initializes the guessesLeft variable', () => {
         expect(gameVariables.guessesLeft).toEqual(11);
+    });
+
+    test('initializes the lettersGuessed variable', () => {
+        expect(gameVariables.lettersGuessed).toEqual([]);
+    });
+
+    test('initializes the array of spells to guess', () => {
+        expect(gameVariables.secretSpell).toEqual(words[0]);
+    });
+
+    test('initializes the letters in the spell', () => {
+        expect(gameVariables.lettersInSpell).toEqual(["f", "o", "o"]);
+    });
+
+    test('initializes the mask for the letters', () => {
+        expect(gameVariables.letterMask).toEqual(["_", "_", "_"]);
+    });
+
+    test('initializes the number of letters left to guess', () => {
+        expect(gameVariables.numLettersLeft).toEqual(3);
     });
 }); 
